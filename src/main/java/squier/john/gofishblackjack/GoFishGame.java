236b42549932ef0a16f56e8fd741eaf5e5b0463b
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-01-29 09:10:26 -0500

    initial commit

--- src/main/java/squier/john/gofishblackjack/GoFishGame.java
+++ src/main/java/squier/john/gofishblackjack/GoFishGame.java
@@ -1,7 +1,12 @@
 package squier.john.gofishblackjack;
 
+import java.util.ArrayList;
+
 /**
  * Created by johnsquier on 1/29/17.
  */
-public class GoFishGame {
+public class GoFishGame
+{
+    private DeckOfCards theDeck;
+    private ArrayList<HandOfCards> players;
 }
